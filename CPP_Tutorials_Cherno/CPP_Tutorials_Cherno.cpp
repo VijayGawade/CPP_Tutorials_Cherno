17ae96a4dd4a31775fb1a3a36f7c23b20e4e9191
Vijay R. Gawade <VijayGawade96@outlook.com>
2022-10-22 09:56:04 +0530

    Add data types in C++

--- CPP_Tutorials_Cherno/CPP_Tutorials_Cherno.cpp
+++ CPP_Tutorials_Cherno/CPP_Tutorials_Cherno.cpp
@@ -3,9 +3,14 @@
 
 #include <iostream>
 
+#include "DataTypes.h"
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    DataTypes *dt = new DataTypes;
+    dt->displayAllDataTypes();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ CPP_Tutorials_Cherno/DataTypes.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include<iostream>
+
+class DataTypes
+{
+	// Charactor data-types
+	char ch = 'A';
+	char numChar = 65;
+
+	// Integert
+	short int a1;
+	int a2;
+	long int a3;
+	long long int a4;
+	unsigned int a5;
+	long a6;
+
+	// deciamal numbers
+	float f1 = 4.5f;
+	double d1 = 56.34;
+	long float lf=42.3f;
+	long double ld=784545.54632119;
+
+	// Boolean
+	bool b1 = true;
+
+public:
+	void displayAllDataTypes() {
+		std::cout << "\n\nChar Data Types:" << std::endl;
+		std::cout << "Size of Char is " << sizeof(char) << std::endl;
+		std::cout << "ch="<<ch << std::endl;
+		std::cout << "numChar="<< numChar << std::endl;
+
+		std::cout << "Size of double is " << sizeof(double) << std::endl;
+		std::cout << "Size of long double is " << sizeof(long double) << std::endl;
+		std::cout << "ld=" << ld << std::endl;
+	}
+};
+
